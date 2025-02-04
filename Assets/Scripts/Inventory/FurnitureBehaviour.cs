089e585b5fe63cb1a8d069d79b25c96da252356c
William Valerie <93768874+WilliamVlr@users.noreply.github.com>
2025-02-04 15:58:50 +0700

    Fixing SO_PlayerAnim.cs, ReadOnlyDrawer.cs to Editor folder, add file ReadOnlyAttribute.cs
    SO_PlayerAnim.cs changed into Runtime and remove namespace UnityEditor

--- Assets/Scripts/Inventory/FurnitureBehaviour.cs
+++ Assets/Scripts/Inventory/FurnitureBehaviour.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using static UnityEditor.Progress;
 
 public class FurnitureBehavior : MonoBehaviour
 {
--- Assets/Scripts/Main_scene/SO_MovingAnim.cs
+++ Assets/Scripts/Main_scene/SO_MovingAnim.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "1", menuName = "ScriptableObjects/MovingAnimator")]
 public class SO_MovingAnim : ScriptableObject
 {
-    public AnimatorController anim;
+    public RuntimeAnimatorController anim;
 }
